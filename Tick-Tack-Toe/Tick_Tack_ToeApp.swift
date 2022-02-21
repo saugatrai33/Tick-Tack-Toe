df5dcc82c24ad9045f5bde4f5caf9d928b990320
Saugat Rai <saugatrai33@gmail.com>
2022-02-22 03:50:39 +0545

    Initial Commit

---
+++ Tick-Tack-Toe/Tick_Tack_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tick_Tack_ToeApp.swift
+//  Tick-Tack-Toe
+//
+//  Created by Evolve on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tick_Tack_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
